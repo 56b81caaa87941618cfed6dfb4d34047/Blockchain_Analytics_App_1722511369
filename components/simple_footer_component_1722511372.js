73ea41c6971a6685b63cf827cb4a1852b33eb826
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-01 16:53:05 +0530

    Placeholder text replacement

--- components/simple_footer_component_1722511372.js
+++ components/simple_footer_component_1722511372.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1722511372", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            Blockchain Analytics Suite
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        Unlock valuable insights from blockchain data with our powerful analytics tools. Stay ahead of the curve in the ever-evolving world of decentralized technology.
                     </div>
                 </div>
             </div>
